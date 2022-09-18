6062505e43fca8397aaef954c2a4302306e8779c
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2022-09-19 00:17:14 +0300

    Ders 11 : For Loop

---
+++ 11_loopsDemo/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        //For Loop
+        int count = 10;
+
+        for (int i = 0; i <= count; i+=2) {
+            System.out.println(i);
+        }
+
+    }
+}
