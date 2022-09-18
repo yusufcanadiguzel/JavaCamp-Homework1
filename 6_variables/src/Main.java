250b1a53764d770708508ff21ba1881484b9c7f1
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2022-09-18 11:56:31 +0300

    Ders 6 : Variables
    Değişkenler nedir, neden ihtiyaç duyulur ve nasıl kullanılır soruları cevaplandı.

---
+++ 6_variables/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        String product = "Bilgisayar";
+
+        int amount = 100;
+
+        System.out.println("Envanterdeki " + product + " sayısı : " + amount);
+
+    }
+}
