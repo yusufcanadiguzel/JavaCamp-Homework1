48ece784cf728dbd44b2d5889ca14afde47a898c
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2022-09-18 12:17:54 +0300

    Ders 7 : Data Types

---
+++ 7_dataTypes/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        byte numberByte = 1;
+        short numberShort = 11;
+        int numberInt = 111;
+        long numberLong = 1111;
+
+        float numberFloat = 2.2f;
+        double numberDouble = 22.22;
+
+        char character = 'Y';
+        String firstName = "Yusufcan";
+
+        boolean isTrue = true;
+
+    }
+}
