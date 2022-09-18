aaf30e2c7684cd7ee2ecf858ffb92b83acdcf1af
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2022-09-18 12:48:56 +0300

    Ders 8 : Conditionals

---
+++ 8_conditionals/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int number = 9;
+
+        if (number == 10) {
+            System.out.println("Sayı 10'a eşittir.");
+        }else if (number > 10) {
+            System.out.println("Sayı 10'dan büyüktür.");
+        }if (number < 10) {
+            System.out.println("Sayı 10'dan küçüktür.");
+        }
+
+    }
+}
