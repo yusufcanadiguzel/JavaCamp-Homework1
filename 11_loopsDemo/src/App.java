fdad902d13efcb49f74f4ae79854574019eebc75
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2022-09-19 09:16:57 +0300

    While Loop

--- 11_loopsDemo/src/App.java
+++ 11_loopsDemo/src/App.java
@@ -2,11 +2,28 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         //For Loop
+        System.out.println("For Loop");
+
         int count = 10;
 
         for (int i = 0; i <= count; i+=2) {
             System.out.println(i);
         }
+
+        System.out.println("------------------------------------------- \n");
+
+
+        //While Loop
+        System.out.println("While Loop");
+
+        int counter = 1;
+
+        while (counter<=10) {
+            System.out.println(counter);
+            counter++;
+        }
+
+        System.out.println("------------------------------------------- \n");
 
     }
 }
