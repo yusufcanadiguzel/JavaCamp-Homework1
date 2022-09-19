4ddcdf9a7313b34e079698dfd02bf4e8c561e3e4
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2022-09-19 15:34:04 +0300

    Ders 15 : Recap Demo 2

---
+++ 15_recapDemo2/src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        double[] numbers = {1.2, 3.4, 5.6, 7.8, 9.0};
+
+        double total = 0;
+        double result = 0;
+
+        for (int i = 0, x = numbers.length; i < x; i++) {
+            System.out.println(numbers[i]);
+
+            total += numbers[i];
+
+            if (numbers[i] > result) {
+                result = numbers[i];
+            }
+        }
+
+        System.out.println("Toplam : " + total);
+        System.out.println("En büyük sayı : " + result);
+
+    }
+}
