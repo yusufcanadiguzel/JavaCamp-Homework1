0a8ce07c10fc86a3025e54c177fc7b738274f0f7
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2022-09-19 12:19:27 +0300

    Ders 13 - Do-While Loop

--- 11_loopsDemo/src/App.java
+++ 11_loopsDemo/src/App.java
@@ -1,29 +1,39 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        //For Loop
+
+        // For Loop
         System.out.println("For Loop");
 
         int count = 10;
 
-        for (int i = 0; i <= count; i+=2) {
+        for (int i = 0; i <= count; i += 2) {
             System.out.println(i);
         }
 
         System.out.println("------------------------------------------- \n");
 
-
-        //While Loop
+        // While Loop
         System.out.println("While Loop");
 
         int counter = 1;
 
-        while (counter<=10) {
+        while (counter <= 10) {
             System.out.println(counter);
             counter++;
         }
 
         System.out.println("------------------------------------------- \n");
 
+        // Do-While Loop
+        System.out.println("Do-While Loop");
+
+        int number = 10;
+
+        do {
+            number++;
+            System.out.println(number);
+        } while (number <= 10);
+
+        System.out.println("------------------------------------------- \n");
     }
 }
