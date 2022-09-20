aa9541be11f363db2ddf8512abdb2d4661580e79
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2022-09-20 17:36:40 +0300

    Ders 23 : Mini Project 4 - Friendly Numbers

---
+++ 22_miniProject4_friendlyNumbers/src/App.java
@@ -0,0 +1,28 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int number1 = 220;
+        int number2 = 284;
+
+        int total1 = 0;
+        int total2 = 0;
+
+        for (int i = 1; i < number1; i++) {
+            if (number1 % i == 0) {
+                total1 += i;
+            }
+        }
+
+        for (int i = 1; i < number2; i++) {
+            if (number2 % i == 0) {
+                total2 += i;
+            }
+        }
+
+        if (total1 == number2 & total2 == number1) {
+            System.out.println("Arkadaş sayılardır.");
+        }else{
+            System.out.println("Arkadaş sayı değillerdir.");
+        }
+    }
+}
